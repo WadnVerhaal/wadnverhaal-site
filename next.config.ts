bd083fe270bbf60f9486f37866d862395f5f53d0
WadnVerhaal <Sander.cannegieter@live.nl>
2026-07-11 20:27:03 +0200

    Allow stable marketing asset host

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ameland-audiotours-website-r1gbm5k5z-wadnverhaals-projects.vercel.app',
+      },
+    ],
+  },
+}
 
 export default nextConfig;
